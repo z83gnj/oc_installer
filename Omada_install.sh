3bbdd514e39a2363994f7ab3a7c8b34546d887c1
Ákos Matiny <akos.matiny@gmail.com>
2025-01-25 19:08:28 +0100

    done the script for testing

--- Omada_install.sh
+++ Omada_install.sh
@@ -6,12 +6,17 @@ omada_url="https://static.tp-link.com/upload/software/2024/202412/20241224/Omada
 # Extract the filename without the extension
 omada_filename=$(basename $omada_url .tar.gz)
 
+# Install dependencies
+apt install -y wget curl gnupg gpg openjdk-17-jre-headless jsvc
+
 # Add mongodb repository
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo gpg --dearmor -o /usr/share/keyrings/mongodb-server-4.4.gpg
 echo "deb [signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg] https://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
-# Install dependencies
-apt install -y wget curl gnupg gpg openjdk-17-jre-headless jsvc mongodb-org
+# Install mongodb
+apt install -y mongodb-org
+systemctl start mongodb
+systemctl enable mongodb
       
 if [ -f libssl1.1_1.1.1w-0+deb11u2_amd64.deb ]; then
   dpkg -i libssl1.1_1.1.1w-0+deb11u2_amd64.deb
@@ -25,20 +30,12 @@ else
   dpkg -i libssl1.1_1.1.1w-0+deb11u2_amd64.deb
 fi
 
-
-          
-sudo apt install -y ./Omada_SDN_Controller_v5.15.6.7_linux_x64.tar.gz
-
-sudo systemctl start mongod
-
-            sudo systemctl enable mongod
-
-            tar -xvzf Omada_SDN_Controller_v5.15.6.7_linux_x64.tar.gz
-
-            cd Omada_SDN_Controller_v5.15.6.7_linux_x64/
-             sudo ./install.sh
+mkdir -p ~/tmp
+cd ~/tmp
 wget $omada_url
 
+tar -xvzf $omada_filename.tar.gz
 
+cd $omada_filename
 
-
+./install.sh
